15fd58ca7fd5567b9f051201fc643acdb5b02dc2
Je Yun <alwaysblue21@gmail.com>
2023-05-17 15:52:10 -0400

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,5 +1,20 @@
 
-if (typeof jQuery == 'undefined'){
-    console.log('oops! I still have to link my jQuery properly!');
-  } else {console.log('I did it! I linked jQuery and this js file!')};
+// if (typeof jQuery == 'undefined'){
+//     console.log('oops! I still have to link my jQuery properly!');
+//   } else {console.log('I did it! I linked jQuery and this js file!')};
+ 
+//   Query for your div with the id of container and set it to a variable named $container
+// console.log $container
+// Create an <h1> element and set it to a variable called $h1 and console log it
+// Add some text inside the h1 element. Example text: 'Hogwarts'
+// Why isn't your $h1 it appearing on your page?
+
+
+  const $container = $("#container");
+  console.log($container);
   
+  const $h1 = $("<h1>");
+  console.log($h1);
+  $h1.text("Hogwarts")
+  $container.append($h1);
+
