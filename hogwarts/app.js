9aef6d7a47edb8cdd65428def1faef7d67a5ff09
Je Yun <alwaysblue21@gmail.com>
2023-05-17 18:05:19 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -10,11 +10,30 @@
 // Why isn't your $h1 it appearing on your page?
 
 
-  const $container = $("#container");
-  console.log($container);
+const $container = $("#container");
+console.log($container);
   
-  const $h1 = $("<h1>");
-  console.log($h1);
-  $h1.text("Hogwarts")
-  $container.append($h1);
+const $h1 = $("<h1>");
+console.log($h1);
+$h1.text("Hogwarts")
+$container.append($h1);
 
+const $h2 = $("<h2>");
+console.log($h2);
+$h2.text("Je Min Yun");
+$container.append($h2);
+
+const $h3 = $("<h3>");
+console.log($h3);
+$h3.text("Gryffindor");
+$container.append($h3);
+
+const $h4 = $("<h4>");
+console.log($h4);
+$h4.addClass("owl").text("Hoot");
+$container.append($h4);
+
+const $h41 = $("<h4>");
+console.log($h41);
+$h41.text("A wand made of holly with a phoenix feather core");
+$container.append($h41);
